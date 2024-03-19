40e0c9320354cd2a63eecd36c388f48beb90eeed
a925610 <a925610@192.168.1.38>
2024-03-19 19:34:08 +0000

    Fisrt version usuarioFactory

--- TareaIoC/src/com/app/tareaioc/UsuarioFactory.java
+++ TareaIoC/src/com/app/tareaioc/UsuarioFactory.java
@@ -1,5 +1,13 @@
 package com.app.tareaioc;
 
 public class UsuarioFactory {
-
+	
+	public void getInsertarUsuario() {
+		System.out.println("Insertar usuario");
+	}
+	
+	public void getEliminarUsuario(int id) {
+		System.out.println("Eliminar usuario: "+id);
+	}
+	
 }
