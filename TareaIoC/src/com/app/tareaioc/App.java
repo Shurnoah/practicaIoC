e42afadcb73a0ac165cc2b8b6da025a1d58fdb47
a925610 <a925610@192.168.1.38>
2024-03-19 19:53:09 +0000

    main Class first version

---
+++ TareaIoC/src/com/app/tareaioc/App.java
@@ -0,0 +1,11 @@
+package com.app.tareaioc;
+
+public class App {
+	
+	public static void main(String[] args) {
+	UsuarioFactory obj = new UsuarioFactory();
+	obj.getInsertarUsuario();
+	obj.getEliminarUsuario(0);
+	}
+	
+}
